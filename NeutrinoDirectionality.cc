3161ff754ae1a35defc381771be069de713d9678
shayshunk <60236335+shayshunk@users.noreply.github.com>
2024-01-03 02:00:38 -0500

    Added a more efficient way of storing and creating the histograms.

--- NeutrinoDirectionality.cc
+++ NeutrinoDirectionality.cc
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <string>
-#include <array>
+
 #include "NeutrinoDirectionality.h"
 
 using std::cout;
@@ -15,10 +13,7 @@ int main()
 	gErrorIgnoreLevel = kError;
 
 	// Defining the 3 directions for calculations
-	array<string, 3> axes{"x", "y", "z"};
-
-	// Set up our output file
-	//TFile *outputFile = new TFile("Directionality.root", "recreate");
+	array<char, 3> axes{'x', 'y', 'z'};
 
 	// Set up what we're measuring. Check enums in header for what the ints are
 	array<float, 5> phi, phiError;
@@ -28,7 +23,25 @@ int main()
 	// Don't need an array for the true reactor direction
 	array<array<TH1D*, 4>, 4> histogram;
 
-	cout << "Checking the enum: " << Data << "\n";
+	// Set up histograms for all 3 directions
+	for (const char &axis : axes)
+	{
+		for (int i = 0; i < 4; i++) // Dataset
+		{
+			for (int j = 0; j < 5; j++) // Signal set
+			{
+				string dataset = DatasetToString(i);
+				string signalSet = SignalToString(j);
+				string direction = string(1, axis);
+				string histogramName = dataset + "_" + signalSet + "_" + axis;
+				histogram[i][j] = new TH1D(histogramName.c_str(), dataset.c_str(), bins, -histogramMax, histogramMax);
+			}
+		}
+	}
+
+	// Set up our output file
+	TFile *outputFile = new TFile("Directionality.root", "recreate");
+	outputFile->Close();
 
 	return 0;
 }
--- NeutrinoDirectionality.h
+++ NeutrinoDirectionality.h
@@ -1,7 +1,11 @@
-#include "TH1D.h"
 #include "TError.h"
 #include "TKey.h"
 #include "TFile.h"
+#include "TH1D.h"
+
+#include <iostream>
+#include <string>
+#include <array>
 
 enum Datasets
 {
@@ -10,6 +14,40 @@ enum Datasets
 
 enum Signals
 {
-    CorrelatedOn = 0, CorrelatedOff, AcceleratedOn, AcceleratedOff
+    CorrelatedReactorOn = 0, CorrelatedReactorOff, AccidentalReactorOn, AccidentalReactorOff, TotalDifference
 };
 
+const int bins = 301; // Number of bins for our histograms
+const float histogramMax = 150.5; // Maximum value of bin for histograms
+
+std::string DatasetToString(int num)
+{
+    std::string name;
+
+    switch (num)
+    {
+        case 0: { name = "Data"; } break;
+        case 1: { name = "DataUnbiased"; } break;
+        case 2: { name = "Sim"; } break;
+        case 3: { name = "SimUnbiased"; } break;
+        case 4: { name = "True"; } break;
+    }
+
+    return name;
+}
+
+std::string SignalToString(int num)
+{
+    std::string name;
+
+    switch (num)
+    {
+        case 0: { name = "CorrelatedReactorOn"; } break;
+        case 1: { name = "CorrelatedReactorOff"; } break;
+        case 2: { name = "AccidentalReactorOn"; } break;
+        case 3: { name = "AccidentalReactorOff"; } break;
+        case 4: { name = "TotalDifference"; } break;
+    }
+
+    return name;
+}
