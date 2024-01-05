33c99b7196eef05314686f44dc7a7b72c7dd48c7
shayshunk <60236335+shayshunk@users.noreply.github.com>
2024-01-05 00:55:05 -0500

    Made the ROOT pointers unique instead of raw.

--- NeutrinoDirectionality.cc
+++ NeutrinoDirectionality.cc
@@ -1,11 +1,7 @@
 
 #include "NeutrinoDirectionality.h"
 
-using std::cout;
-using std::string;
-using std::array;
-using std::ifstream;
-using std::getline;
+using std::cout, std::string, std::ifstream, std::array, std::getline;
 
 int main()
 {
@@ -21,7 +17,7 @@ int main()
 
 	// Need histograms for counting each variable. Check enums in header for what the ints are
 	// Don't need an array for the true reactor direction
-	array<array<TH1D*, 4>, 4> histogram;
+	array<array<std::unique_ptr<TH1D>, 4>, 4> histogram;
 
 	// Set up histograms for all 3 directions
 	for (const char &axis : axes)
@@ -34,13 +30,13 @@ int main()
 				string signalSet = SignalToString(j);
 				string direction = string(1, axis);
 				string histogramName = dataset + "_" + signalSet + "_" + axis;
-				histogram[i][j] = new TH1D(histogramName.c_str(), dataset.c_str(), bins, -histogramMax, histogramMax);
+				histogram[i][j] = std::make_unique<TH1D>(histogramName.c_str(), dataset.c_str(), bins, -histogramMax, histogramMax);
 			}
 		}
 	}
 
 	// Set up our output file
-	TFile *outputFile = new TFile("Directionality.root", "recreate");
+	std::unique_ptr<TFile> outputFile = std::make_unique<TFile>("Directionality.root", "recreate");
 	outputFile->Close();
 
 	return 0;
